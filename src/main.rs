73be1f548ee50e27a30097dbef040b329e2ee097
decker <mdatmosci@gmail.com>
2024-11-08 13:16:06 -0600

    added function to return HashMap from vec then find and print the mode

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,6 @@ fn partition_vec(v: &mut [i32]) -> usize {  //pass in mutable slice, return usiz
 //quicksort function
 //set the pivot with partition then call quick sort on two subslices
 fn quick_sort(v: &mut [i32]) {  //don't return anything just call recursively
-
     //ensure we have a valid vector to know when to stop
     if v.len() <= 1 {
         return
@@ -49,23 +48,36 @@ fn quick_sort(v: &mut [i32]) {  //don't return anything just call recursively
     quick_sort(&mut v[pivot + 1..]); //end half
 }
 
+
+//function to convert vec to a hash map
+//keys with be the values in the vector
+//count up each time we find that key
+use std::collections::HashMap;  //bring hashmap into scope
+
+fn hashmap_from_vec(v: &mut [i32]) -> HashMap <&mut i32, i32> {  //v does not have to be mutable
+                                                                 //but it is
+    let mut map = HashMap::new();
+    //loop over vector adding
+    for i in v {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    return map  //return the map
+
+}
+
 fn main() {
 
     //create vector using vec! macro
     //
-    let mut vec = vec![1, -6, 6, 76, 98, -75, 104, 3, 0, 3, 25, 4, 8, 19];
-
-    //first we need to sort the vector
-    //test partition function
-    let k = partition_vec(&mut vec);
-    println!("{k}");
-
+    let mut vec = vec![1, -6, 6, 76, 98, -75, 104, 3, 0, 3, 25, 4, 8, 19, -6, 6, 1, 1, 1];
+    //output vec
     for v in &vec {
         println!("{v}");
     }
 
     quick_sort(&mut vec);
-
     println!("sould be sorted");
     for v in &vec {
         println!("{v}");
@@ -73,10 +85,27 @@ fn main() {
 
     //now that is is sorted we just find value at middle index
     let len = vec.len() / 2;
-
     let median = &vec[len];
-
     println!("median is {median}");
 
+    //make a hashmap to count occurances
+    let map = hashmap_from_vec(&mut vec);
+
+    println!("{map:?}");
+
+    //find the max value in the hashmap
+    let mut max_count = 0; 
+    for (key, count) in &map {  //use reference
+        if *count > max_count {  //must deref
+            max_count = *count;
+        }
+    }
+    println!("max count is {max_count}");
+    //print out key, count for max_count
+    for (key, count) in &map {  //ref to map
+        if *count == max_count {  //must deref due to using r3ef to map
+            println!("The mode is {key} with a count of {max_count}");
+        }
+    }
 
 }
