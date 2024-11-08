effdc95603869d06d6c3c138d6b7ab3ae81896ba
decker <mdatmosci@gmail.com>
2024-11-08 11:31:24 -0600

    put problem definition in main.rs

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//Given a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+//mark decker Nov 8 2024
+//
+
+fn main() {
+    println!("Hello, world!");
+}
